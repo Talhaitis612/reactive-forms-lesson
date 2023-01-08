c4c18938856b7292894164827819179aa020a199
Talhaitis612 <bsem-f18-206@superior.edu.pk>
2023-01-08 21:55:43 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-forms-lesson';
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+
+  signupForm! : FormGroup;
+  
+  constructor(){}
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      'username' : new FormControl(null),
+      'email' : new FormControl(null),
+      'gender' : new FormControl('male')
+
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
 }

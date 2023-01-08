1385eb9a824c02876136e0e49882a3491f56d861
Talhaitis612 <bsem-f18-206@superior.edu.pk>
2023-01-08 23:03:12 +0500

    form array addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +14,25 @@ export class AppComponent implements OnInit{
   constructor(){}
   ngOnInit(){
     this.signupForm = new FormGroup({
-      'username' : new FormControl(null),
-      'email' : new FormControl(null),
-      'gender' : new FormControl('male')
+      'userData' : new FormGroup({
+        'username' : new FormControl(null, Validators.required),
+        'email' : new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      'gender' : new FormControl('male'),
+      'hobbies' : new FormArray([])
 
     })
   }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
+  
 
   onSubmit(){
     console.log(this.signupForm);

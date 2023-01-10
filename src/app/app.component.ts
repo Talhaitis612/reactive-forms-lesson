76d634d85bba7fd8837bb5f3217302e01bff1eac
Talhaitis612 <bsem-f18-206@superior.edu.pk>
2023-01-10 22:27:28 +0500

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,27 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData' : new FormGroup({
-        'username' : new FormControl(null, Validators.required),
-        'email' : new FormControl(null, [Validators.required, Validators.email]),
+        'username' : new FormControl(null, [Validators.required, Validators.minLength(5)]),
+        'email' : new FormControl(null, [Validators.required, Validators.email], ),
       }),
       'gender' : new FormControl('male'),
       'hobbies' : new FormArray([])
 
     })
+    // this.signupForm.setValue({
+    //   'userData' : {
+    //     'username' : 'Talha@123',
+    //     'email' : 'luffy@gmail.com'
+    //   },
+    //   'gender' : 'male',
+    //   'hobbies' : []
+    // });
+    this.signupForm.patchValue({
+      'userData' : {
+        'email' : 'luffy@gmail.com'
+      },
+
+    });
   }
 
   onAddHobby(){
@@ -37,4 +52,19 @@ export class AppComponent implements OnInit{
   onSubmit(){
     console.log(this.signupForm);
   }
+
+  forbiddenEmail(control : FormControl) : Promise<any> | Observable<any>{
+     const promise = new Promise<any>((resolve, reject)=>{
+      setTimeout(()=>{
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbidden' : true})
+        }
+        else {
+          resolve(null);
+        }
+      },1500);
+     });
+     return promise;
+  }
+ 
 }
